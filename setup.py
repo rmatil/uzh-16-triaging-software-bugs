f4ba7243123e9e9f91c990e6aa5435628675a8af
Raphael Matile <raphael.matile@gmail.com>
2016-12-06 22:50:41 +0100

    Add database schema creation

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import os.path as path
 from csv_cleaner import CsvCleaner
+from persistence import SqLitePersister
 
 source = "resources/eclipse/cc.csv"
 target = "resources/eclipse/cc_cleaned.csv"
+database = "resources/database/bug_reports.db"
+
+# create absolute paths
+source = path.abspath(source)
+target = path.abspath(target)
+database = path.abspath(database)
 
 # only create the cleaned CC csv if not existing yet
 if not path.isfile(target):
     CsvCleaner.clean_cc(source, target)
+
+# create database if not yet existing
+persister = SqLitePersister(database)
+
+if not path.isfile(database):
+    persister.create_db()
+    persister.setup_db_scheme()
+

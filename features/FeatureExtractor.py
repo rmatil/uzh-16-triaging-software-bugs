d8d6003cce3c3b2348784fcf4c040a61a25f4a68
Raphael Matile <raphael.matile@gmail.com>
2016-12-07 18:24:33 +0100

    Add feature 6

--- features/FeatureExtractor.py
+++ features/FeatureExtractor.py
@@ -10,7 +10,7 @@ SUCCESSRATE = ('SELECT success.who, success.count*1.0 / (success.count+ fail.cou
                'current_status == "CLOSED" AND reports.current_resolution == "FIXED"'
                'GROUP BY assigned_to.who) AS success'
                'JOIN ('
-               'SELECT assigned_to.who, count(assigned_to.who) AS count from reports'
+               'SELECT assigned_to.who, count(assigned_to.who) AS count FROM reports'
                'JOIN assigned_to ON reports.bug_id = assigned_to.bug_id'
                'WHERE current_status == "RESOLVED" AND reports.current_resolution == "WONTFIX" OR'
                'current_status == "RESOLVED" AND reports.current_resolution == "INVALID" OR'
@@ -19,7 +19,7 @@ SUCCESSRATE = ('SELECT success.who, success.count*1.0 / (success.count+ fail.cou
                'current_status == "VERIFIED" AND reports.current_resolution == "WONTFIX" OR'
                'current_status == "VERIFIED" AND reports.current_resolution == "INVALID"'
                'GROUP BY assigned_to.who) AS fail'
-               'ON success.who = fail.who;'
+               'ON success.who = FAIL.who;'
                )
 
 # 5. Number of reopenings per bug
@@ -27,5 +27,18 @@ REOPENINGS = ('SELECT bug_status.bug_id, bug_status.timestamp, COUNT(bug_status.
               'FROM bug_status'
               'WHERE bug_status.what = "REOPENED"'
               'GROUP BY bug_id'
-              'ORDER BY count DESC'
+              'ORDER BY count DESC;'
               )
+
+# 6. Time during which the bug was open
+OPEN_TIME = ('SELECT reports.bug_id, minmax.min, minmax.max, (minmax.max - minmax.min) AS difference FROM ('
+             'SELECT'
+             'bug_id,'
+             'MIN(timestamp) AS min,'
+             'MAX(timestamp) AS max'
+             'FROM resolution'
+             'GROUP BY bug_id'
+             'ORDER BY bug_id, timestamp'
+             ') AS minmax INNER JOIN reports ON minmax.bug_id = reports.bug_id'
+             'WHERE reports.current_status IN ("CLOSED")'
+             )

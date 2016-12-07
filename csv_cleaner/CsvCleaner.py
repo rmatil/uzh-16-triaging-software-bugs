61f00f133a7b6b7ae2af29c9d436c2a8508bbc40
yvste <Yves@origin.local>
2016-12-07 01:00:15 +0100

    fixed CsvCleaner
    didn’t delete more than 1 elements

--- csv_cleaner/CsvCleaner.py
+++ csv_cleaner/CsvCleaner.py
@@ -35,13 +35,14 @@ class CsvCleaner(object):
             # assume, the "what" field has multiple addresses in it
             if length_row > 4:
                 for x in xrange(2, 2 + length_row - 4):
-                    str = str + ';' + row[x].strip()
+                    str = str + ',' + row[x].strip()
 
                 row[1] = str
                 row[2] = row[2 + length_row - 4]
                 row[3] = row[3 + length_row - 4]
 
-                del row[4 - length_row]
+                for x in xrange(0,length_row-4):
+                    del row[-1]
 
             # write (cleaned) row
             writer.writerow(row)

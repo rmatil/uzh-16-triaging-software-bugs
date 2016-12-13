f07ca72e826d6064b7afcddd25a1ae04bd5efdd9
Raphael Matile <raphael.matile@gmail.com>
2016-12-13 10:57:04 +0100

    Generate feature 3-10 and add also some documentation

--- features/FeatureImporter.py
+++ features/FeatureImporter.py
@@ -21,46 +21,57 @@ class FeatureImporter(SqLiteConnector.SqLiteConnector):
         self._insert_many("INSERT INTO %s (bug_id) VALUES (?)" % table, rows)
 
     def generate_and_import_feature_1(self, table):
+        """Calculate and insert feature 1: Success Rate of Bug Assignee"""
         rows = self._fetch_many(featureQueries.SUCCESS_RATE)
         self._insert_many('UPDATE %s SET feature_1 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_2(self, table):
+        """Calculate and insert feature 2: Reputation of Bug Reporter"""
         rows = self._fetch_many(featureQueries.REPUTATION_RATE)
         self._insert_many('UPDATE %s SET feature_2 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_3(self, table):
+        """Calculate and insert feature 3: Ratio of Success of a bug report for every reporter-assignee pair"""
         rows = self._fetch_many(featureQueries.REPORTER_ASSIGNEE_RATE)
         self._insert_many('UPDATE %s SET feature_3 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_4(self, table):
+        """Calculate and insert feature 4: Impact of reassignments of a bug"""
         rows = self._fetch_many(featureQueries.REASSINGMENTS)
         self._insert_many('UPDATE %s SET feature_4 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_5(self, table):
+        """Calculate and insert feature 5: Number of reponenings of a bug"""
         rows = self._fetch_many(featureQueries.REOPENINGS)
         self._insert_many('UPDATE %s SET feature_5 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_6(self, table):
+        """Calculate and insert feature 6: Time during which the bug was open"""
         rows = self._fetch_many(featureQueries.OPEN_TIME)
         self._insert_many('UPDATE %s SET feature_6 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_7(self, table):
+        """Calculate and insert feature 7: Software module to which bug was assigned to"""
         rows = self._fetch_many(featureQueries.SOFTWARE_MODULE)
         self._insert_many('UPDATE %s SET feature_7 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_8(self, table):
+        """Calculate and insert feature 8: Relationship between reporter and Users in CC"""
         rows = self._fetch_many(featureQueries.RELATIONSHIP)
         self._insert_many('UPDATE %s SET feature_8 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_9(self, table):
+        """Calculate and insert feature 9: Relation of Software Version and Bug Fix"""
         rows = self._fetch_many(featureQueries.SOFTWARE_VERSION)
         self._insert_many('UPDATE %s SET feature_9 = ? WHERE bug_id = ?' % table, rows)
 
     def generate_and_import_feature_10(self, table):
+        """Calculate and insert feature 10: Influence of a bug relation to user interface environment and network"""
         rows = self._fetch_many(featureQueries.BUGNATURE)
         self._insert_many('UPDATE %s SET feature_10 = ? WHERE bug_id = ?' % table, rows)
 
     def _fetch_many(self, query):
+        """Fetches all rows of the given query"""
         if self._connection is None:
             raise Exception("Open database connection first")
 
@@ -71,6 +82,7 @@ class FeatureImporter(SqLiteConnector.SqLiteConnector):
         return cursor.fetchall()
 
     def _insert_many(self, query, data):
+        """Inserts the data using the given query"""
         if self._connection is None:
             raise Exception("Open database connection first")
 
